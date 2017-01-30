ba2cecf7b7b5d4242b6b9e1d949b6b56e52f5d43
TMerrick55 <tmerrick44@gmail.com>
2017-01-30 15:30:46 -0500

    In-class Redo of Bash HW, Part 2: Ver. 1.0

---
+++ 2nd_Raffle_Ver._2.rb
@@ -0,0 +1,12 @@
+def close_but_not_quite(winning_num, ticket_num)
+	matches = 0
+	index_position = 0
+
+	ticket_num.length.times do
+		if ticket_num[index_position] == winning_num[index_position]
+			matches += 1
+		end
+		index_position += 1
+	end
+	winning_num.length - matches == 1
+end
